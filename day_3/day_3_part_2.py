dc3c5b42546d28580aa90973f2d727922ad42f08
Timo van Praagh <timo.van.praagh@nl.ey.com>
2024-12-03 09:50:33 +0100

    adding some comments to day_3 code

--- day_3/day_3_part_2.py
+++ day_3/day_3_part_2.py
@@ -1,7 +1,7 @@
 import re
 
 
-def read_lines(path: str = "./input_day_x.txt"):
+def read_lines(path: str = "./input_day_3.txt"):
     with open(path) as f:
         lines = []
         lines = f.readlines()
@@ -11,16 +11,24 @@ def read_lines(path: str = "./input_day_x.txt"):
     return [line for line in lines]
 
 
-lines = read_lines("./input_day_x_example.txt")
+lines = read_lines("./input_day_3_example.txt")
+# I removed newlines by hand from the input.
 lines = read_lines()
 big_line = "".join(lines)
+# check if bigline now really is just one line with no hidden
+# new line seperators:
+assert len(big_line.splitlines()) == 1
 sum_mul = 0
+# add newline characters for each do and dont section (and a beginning section)
 seperate_do_dont = big_line.replace("do()", "\ndo()").replace("don't()", "\ndon't()")
+# now create a list of lines for each new line
+# and filter out those that start with don't
 do_lines = [
     dox_line
     for dox_line in seperate_do_dont.splitlines()
     if not dox_line.startswith("don't()")
 ]
+# go through accepted lines and do the same as `day_1_part_1.py`
 for do_line in do_lines:
     muls = re.findall(r"mul\(\d+\,\d+\)", do_line)
     for mul in muls:
@@ -29,4 +37,5 @@ for do_line in do_lines:
         sum_mul = sum_mul + multiple
 
 print(sum_mul)
+assert sum_mul == 89349241
 print("done")
